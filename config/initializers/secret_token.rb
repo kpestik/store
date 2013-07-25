531ae4e5082d3c0a11c96355d7c6d9f04bf13dc6
Tsar <tsar@bomba.(none)>
2013-07-25 16:46:40 -0400

    Started project and installed spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '5417d839d58cc47967b891a71c28e590fbf84ac21dbc850ab02355f03d937024f10c15da7cfa995b03cddf6327a9e6459665e87a66380516f96ec8706cb876b1'
